0c0f3294279c71cdd1788f414e7560eb85686cdf
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 11:47:45 +0530

    REFACTOR: corrected attendance function name

--- src/com/employeewagecompute20220823/Employee.java
+++ src/com/employeewagecompute20220823/Employee.java
@@ -2,7 +2,7 @@ package com.employeewagecompute20220823;
 
 public class Employee {
 	
-	public int Attendance () {
+	public int attendance () {
 		return (int) Math.floor(Math.random() * 10) % 2;
-	}
+	} 
 }
--- src/com/employeewagecompute20220823/EmployeeWageCompute20220823.java
+++ src/com/employeewagecompute20220823/EmployeeWageCompute20220823.java
@@ -8,7 +8,7 @@ public class EmployeeWageCompute20220823 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Employee employee = new Employee();
-		if (IS_PRESENT == employee.Attendance()) {
+		if (IS_PRESENT == employee.attendance()) {
 			System.out.println("Employee is present");
 		} else {
 			System.out.println("Employee is absent");
