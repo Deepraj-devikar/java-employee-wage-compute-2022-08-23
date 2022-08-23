4ec5d681f801dc91cd289101350ffccda7cb2fed
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 11:36:37 +0530

    ADDED: Employee With Attendance Functionality

---
+++ src/com/employeewagecompute20220823/Employee.java
@@ -0,0 +1,8 @@
+package com.employeewagecompute20220823;
+
+public class Employee {
+	
+	public int Attendance () {
+		return (int) Math.floor(Math.random() * 10) % 2;
+	}
+}
--- src/com/employeewagecompute20220823/EmployeeWageCompute20220823.java
+++ src/com/employeewagecompute20220823/EmployeeWageCompute20220823.java
@@ -1,9 +1,18 @@
 package com.employeewagecompute20220823;
 
-public class EmployeeWageCompute20220823 {
+import com.employeewagecompute20220823.Employee;
 
+public class EmployeeWageCompute20220823 {
+	static final int IS_PRESENT = 1;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Employee employee = new Employee();
+		if (IS_PRESENT == employee.Attendance()) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
