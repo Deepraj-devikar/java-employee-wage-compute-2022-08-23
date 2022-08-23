f217c6349dd3cfd00bba0850511ef7bf64974dd8
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 13:07:11 +0530

    ADDED: Employee Monthly Hour Limit

--- src/com/employeewagecompute20220823/Employee.java
+++ src/com/employeewagecompute20220823/Employee.java
@@ -7,9 +7,11 @@ public class Employee {
 	static public float FULL_DAY_HOUR = 8;
 	static public float PART_TIME_HOUR = 4;
 	static public int MONTHLY_WORKING_DAYS = 20;
+	static public float MONTHLY_HOUR_LIMIT = 25;
 	
 	static public int day = 0;
 	static public float monthlyWage = 0;
+	static public float monthlyHour = 0;
 	
 	public int attendance () {
 		int result = (int) Math.floor(Math.random() * 10) % 3;
@@ -30,8 +32,10 @@ public class Employee {
 	public float dailyWage() {
 		switch (this.attendance()) {
 		case IS_PRESENT:
+			monthlyHour += FULL_DAY_HOUR;
 			return Math.round((float) WAGE_PER_HOUR * FULL_DAY_HOUR * 100) / 100;
 		case IS_PART_TIME:
+			monthlyHour += PART_TIME_HOUR;
 			return Math.round((float) WAGE_PER_HOUR * PART_TIME_HOUR * 100) / 100;
 		default:
 			return (float) 0.00;
@@ -39,7 +43,7 @@ public class Employee {
 	}
 	
 	public float monthlyWage() {
-		while (this.day < MONTHLY_WORKING_DAYS) {
+		while (this.day < MONTHLY_WORKING_DAYS && monthlyHour <= MONTHLY_HOUR_LIMIT) {
 			this.day++;
 			this.monthlyWage += dailyWage();
 		}
